b9c9796cce177811ea447e61dd9ce47aa453bdef
Osifo John <osifojohntec@gmail.com>
2023-08-05 14:08:58 +0100

    useronboarding implemented

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -1,8 +1,10 @@
 import { Request, Response } from 'express';
 import User, { validateUser } from '../models/user';
-import { IOrganisation, IUserRequestAdmin, STATUSCODE } from '../types';
+import { ILogin, IOrganisation, IUserRequestAdmin, STATUSCODE } from '../types';
 import { validateOrganisation } from '../models/organisation';
 import Organisation from '../models/organisation';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 // @desc Create new organisation
 // @route POST /organisation
@@ -53,8 +55,8 @@ export const organisationRegister = async (
       });
     }
   } catch (error) {
-    res.status(STATUSCODE.BAD_REQUEST).json({
-      message: 'Invalid organisation data received',
+    res.status(STATUSCODE.SERVER_ERROR).json({
+      message: 'Internal or server error',
     });
   }
 };
@@ -62,7 +64,7 @@ export const organisationRegister = async (
 // @desc Create new admin
 // @route POST /users
 // @access public
-export const adminSignup = async (
+export const signup = async (
   req: Request<unknown, unknown, IUserRequestAdmin>,
   res: Response
 ) => {
@@ -84,23 +86,33 @@ export const adminSignup = async (
       .json({ error: error.details[0].message });
   }
 
-  const duplicateUserEmail = await User.findOne({ email });
-  if (duplicateUserEmail) {
+  const duplicateUser = await User.findOne({ email });
+  if (duplicateUser) {
+    res.status(STATUSCODE.BAD_REQUEST);
     return res
       .status(STATUSCODE.BAD_REQUEST)
       .json({ message: 'Email already taken' });
   }
 
+  const hashedPwd = await bcrypt.hash(password, 10);
+
   try {
     const retrivedOrganisation = await Organisation.findById(
       organisation
     ).exec();
+
+    if (!retrivedOrganisation) {
+      return res
+        .status(STATUSCODE.BAD_REQUEST)
+        .json({ message: 'Invalid  details received' });
+    }
+
     if (retrivedOrganisation) {
       const user = await User.create({
         firstName,
         lastName,
         email,
-        password,
+        password: hashedPwd,
         phone,
         organisation,
         role: 'admin',
@@ -113,14 +125,64 @@ export const adminSignup = async (
         : res.status(STATUSCODE.BAD_REQUEST).json({
             message: 'Invalid personal details received',
           });
-    } else {
-      res.status(STATUSCODE.BAD_REQUEST).json({
-        message: 'Invalid personal details received',
-      });
     }
   } catch (error) {
-    res.status(STATUSCODE.BAD_REQUEST).json({
-      message: 'Invalid personal details received',
+    res.status(STATUSCODE.SERVER_ERROR).json({
+      message: 'Internal or server error',
     });
+  }
+};
+
+// @desc Login user
+// @route POST /login
+// @access public
+export const login = async (
+  req: Request<unknown, unknown, ILogin>,
+  res: Response
+) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res
+      .status(STATUSCODE.BAD_REQUEST)
+      .json({ message: 'Please enter all field values' });
+  }
+
+  try {
+    const foundUser = await User.findOne({ email }).exec();
+
+    if (!foundUser || !foundUser.active) {
+      return res
+        .status(STATUSCODE.UNAUTHORIZED)
+        .json({ message: 'Unauthorized' });
+    }
+
+    const match = await bcrypt.compare(password, foundUser.password);
+
+    if (!match)
+      return res
+        .status(STATUSCODE.UNAUTHORIZED)
+        .json({ message: 'Unauthorized' });
+
+    if (foundUser && match) {
+      const accessToken = jwt.sign(
+        {
+          user: {
+            firstName: foundUser.firstName,
+            email: foundUser.email,
+            id: foundUser.id,
+          },
+        },
+        process.env.ACCESS_TOKEN_SECRET as string,
+        { expiresIn: '15m' }
+      );
+      res.status(200).json({ accessToken });
+    } else {
+      res
+        .status(STATUSCODE.UNAUTHORIZED)
+        .json('email or password is not valid');
+    }
+  } catch (error) {
+    res.status(STATUSCODE.SERVER_ERROR).json('Internal or server error');
   }
 };
